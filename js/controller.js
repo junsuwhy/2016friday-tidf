721ed4007a434572158e4c461d8955cb75994171
junsuwhy <junsuwhy@gmail.com>
2016-03-30 12:37:53 +0800

    Upload html files.

---
+++ js/controller.js
@@ -0,0 +1,8 @@
+var host = 'http://game.doyouaflavor.tw/';
+
+
+var app = angular.module('controller',['ngSanitize']);
+app.controller('MainCtrl', [function () {
+   
+}]);
+
---
+++ js/data.js
@@ -0,0 +1,15 @@
+function getImgPath(i){
+    return function(){
+        return 'img/movie';
+    }
+}
+
+var data = [
+    {
+        "title":"abc",
+        "director":"XXX",
+        "award":"HAHAHA",
+        "picture1":
+    }
+            
+    ];

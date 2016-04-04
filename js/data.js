24d08170b9e87f776ce31b80fcfdb321175ff4d0
junsuwhy <junsuwhy@gmail.com>
2016-04-04 09:55:09 +0800

    Add elements.

--- js/data.js
+++ js/data.js
@@ -6,10 +6,79 @@ function getImgPath(i){
 
 var data = [
     {
-        "title":"abc",
+        "title":"金城小子",
         "director":"XXX",
         "award":"HAHAHA",
-        "picture1":
-    }
+        "description":"同步榮獲第48屆金馬獎最佳紀錄片;從 小離鄉的知名畫家劉小東,30年後決 定重歸故里,重新面對鄉愁",
+        "imgBig":"",
+        "imgSmall":"",
+        "youtubeID":"",
+        "link":"http://video.friday.tw/movie/detail/10/2399",
+    },{
+        "title":"艾未未．草泥馬",
+        "director":"XXX",
+        "award":"HAHAHA",
+        "imgBig":"",
+        "imgSmall":"",
+        "youtubeID":"",
+        "link":"http://video.friday.tw/movie/detail/1163",
+    },{
+        "title":"黑晝記",
+        "director":"XXX",
+        "award":"HAHAHA",
+        "imgBig":"",
+        "imgSmall":"",
+        "youtubeID":"",
+        "link":"http://video.friday.tw/movie/detail/6454",
+    },{
+        "title":"再會吧！1999",
+        "director":"XXX",
+        "award":"HAHAHA",
+        "imgBig":"",
+        "imgSmall":"",
+        "youtubeID":"",
+        "link":"http://video.friday.tw/movie/detail/6453",
+    },{
+        "title":"部落之音",
+        "director":"XXX",
+        "award":"HAHAHA",
+        "imgBig":"",
+        "imgSmall":"",
+        "youtubeID":"",
+        "link":"http://video.friday.tw/movie/detail/6452",
+    },{
+        "title":"第四公民",
+        "director":"XXX",
+        "award":"HAHAHA",
+        "imgBig":"",
+        "imgSmall":"",
+        "youtubeID":"",
+        "link":"http://video.friday.tw/movie/detail/6055",
+    },{
+        "title":"哈瑪斯之子",
+        "director":"XXX",
+        "award":"HAHAHA",
+        "imgBig":"",
+        "imgSmall":"",
+        "youtubeID":"",
+        "link":"http://video.friday.tw/movie/detail/2986",
+    },{
+        "title":"太陽•不遠",
+        "director":"XXX",
+        "award":"HAHAHA",
+        "imgBig":"",
+        "imgSmall":"",
+        "youtubeID":"",
+        "link":"http://video.friday.tw/movie/detail/566",
+    },{
+        "title":"蘇富比偽畫大師",
+        "director":"XXX",
+        "award":"HAHAHA",
+        "imgBig":"",
+        "imgSmall":"",
+        "youtubeID":"",
+        "link":"http://video.friday.tw/movie/detail/1962",
+    },
+    
             
     ];
